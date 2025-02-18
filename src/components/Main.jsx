8bf8cd52c875e2be5ba711b037acb15109a86b14
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-02-18 20:05:43 +0100

    exercise finished

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,16 @@
+import SectionBuy from "./SectionBuy"
+
 const Main = () => {
     return (
-        <main>
-            <div className="container-80">
-                <h2> --Content goes here-- </h2>
-            </div>
-            
-        </main>
+            <main>
+                <section className="section-black">
+                    <div className="container-80">
+                        <h2> --Content goes here-- </h2>
+                    </div>
+                </section>
+
+                <SectionBuy />
+            </main>
     )
 }
 export default Main
---
+++ src/components/SectionBuy.jsx
@@ -0,0 +1,43 @@
+const SectionBuy = () => {
+    return (
+        <section className="section-azzure">
+            <div className="container-80">
+                <div className="container-buy">
+                    <div>
+                        <figure>
+                            <img src="../src/assets/img/buy-comics-digital-comics.png" alt="" />
+                        </figure>
+                        <span>digital comics</span>
+                    </div>
+                    <div>
+                        <figure>
+                            <img src="../src/assets/img/buy-comics-merchandise.png" alt="" />
+                        </figure>
+                        <span>DC MERCHANDISE</span>
+                    </div>
+                    <div>
+                        <figure>
+                            <img src="../src/assets/img/buy-comics-shop-locator.png" alt="" />
+                        </figure>
+                        <span>SUBSCRIPTION</span>
+                    </div>
+                    <div>
+                        <figure>
+                            <img src="../src/assets/img/buy-comics-subscriptions.png" alt="" />
+                        </figure>
+                        <span>COMK SHOP LOCATOT</span>
+                    </div>
+                    <div>
+                        <figure>
+                            <img src="../src/assets/img/buy-dc-power-visa.svg" alt="" />
+                        </figure>
+                        <span>BC POWER VISA</span>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
+export default SectionBuy
