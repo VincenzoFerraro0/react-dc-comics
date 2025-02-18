b6d4985f6fd6eef99a9a2ba6bcc7d16d685dcb07
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-02-18 19:36:12 +0100

    component Footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,87 @@
 const Footer = () => {
     return (
         <footer>
-            <h1>footer</h1>
+            <section className="footer-top">
+                <div className="container-menu container-80 ">
+                    <div className="menu-links">
+                        <div>
+                            <h3>DC COMICS</h3>
+                            <ul>
+                                <li><a href="">Characters</a></li>
+                                <li><a href="">Comics</a></li>
+                                <li><a href="">TV</a></li>
+                                <li><a href="">Games</a></li>
+                                <li><a href="">videos</a></li>
+                                <li><a href="">News</a></li>
+                            </ul>
+                            <h3>SHOP</h3>
+                            <ul>
+                                <li><a href="">Shop DC</a></li>
+                                <li><a href="">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>DC</h3>
+                            <ul>
+                                <li><a href="">Terms Of Use</a></li>
+                                <li><a href="">Privacy policy (New)</a></li>
+                                <li><a href="">Ad Choices</a></li>
+                                <li><a href="">Advertising</a></li>
+                                <li><a href="">Subscriptions</a></li>
+                                <li><a href="">Talent Workshops</a></li>
+                                <li><a href="">CPSC Certificates</a></li>
+                                <li><a href="">Ratings</a></li>
+                                <li><a href="">Shop Help</a></li>
+                                <li><a href="">Contact Us</a></li>
+
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>SITES</h3>
+                            <ul>
+                                <li><a href="">DC</a></li>
+                                <li><a href="">MAD Magazine</a></li>
+                                <li><a href="">DC Kids</a></li>
+                                <li><a href="">DC Universe</a></li>
+                                <li><a href="">DC Power Visa</a></li>
+                            </ul>
+                        </div>
+
+
+
+                    </div>
+                    <div className="logo-pattern">
+
+                    </div>
+                </div>
+            </section>
+            <section className="footer-bottom">
+                <div className="container-bottom container-80">
+                    <div className="btn-singUp">
+                        <a href="">SIGN-UP NOW!</a>
+                    </div>
+
+                    <div className="container-icon">
+                        <span>FOLLOW US</span>
+                        <figure>
+                            <img src="../src/assets/img/footer-facebook.png" alt="" />
+                        </figure>
+                        <figure>
+                            <img src="../src/assets/img/footer-twitter.png" alt="" />
+                        </figure>
+                        <figure>
+                            <img src="../src/assets/img/footer-youtube.png" alt="" />
+                        </figure>
+                        <figure>
+                            <img src="../src/assets/img/footer-pinterest.png" alt="" />
+                        </figure>
+                        <figure>
+                            <img src="../src/assets/img/footer-periscope.png" alt="" />
+                        </figure>
+                    </div>
+                </div>
+
+            </section>
         </footer>
     )
 }
