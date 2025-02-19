ca8b0c21f4224bd003e52a3746cb0b1098421f5b
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-02-19 17:21:36 +0100

    add array in App.jsx and add porps in Headers

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,22 @@ import Main from "./components/Main"
 import Footer from "./components/Footer"
 
 function App() {
-
+      //CENTRALIZZAZIONE DEI DATI
+      const links = [
+        { id: 1, text: 'Characters', url: '#', current: true },
+        { id: 2, text: 'Comics', url: '#', current: false },
+        { id: 3, text: 'Movies', url: '#', current: false },
+        { id: 4, text: 'Tv', url: '#', current: false },
+        { id: 5, text: 'Games', url: '#', current: false },
+        { id: 6, text: 'Collectibles', url: '#', current: false },
+        { id: 7, text: 'Videos', url: '#', current: false },
+        { id: 8, text: 'Fans', url: '#', current: false },
+        { id: 9, text: 'News', url: '#', current: false },
+        { id: 10, text: 'Shop', url: '#', current: false }
+    ]
   return (
     <>
-        <Header />
+        <Header links={links} />
         <Main />
         <Footer />
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,9 @@
 import NavBar from "./NavBar"
 
-function Header() {
+function Header(props) {
 
-    //CENTRALIZZAZIONE DEI DATI
-    const links = [
-        { id: 1, text: 'Characters', url: '#', current: true },
-        { id: 2, text: 'Comics', url: '#', current: false },
-        { id: 3, text: 'Movies', url: '#', current: false },
-        { id: 4, text: 'Tv', url: '#', current: false },
-        { id: 5, text: 'Games', url: '#', current: false },
-        { id: 6, text: 'Collectibles', url: '#', current: false },
-        { id: 7, text: 'Videos', url: '#', current: false },
-        { id: 8, text: 'Fans', url: '#', current: false },
-        { id: 9, text: 'News', url: '#', current: false },
-        { id: 10, text: 'Shop', url: '#', current: false }
-    ]
+    const { links } = props
+
     return (
         <header>
             <NavBar links={links} />
